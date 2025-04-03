f463019a5f5841a02cec73df73bab148d76ed82e
morainekhy <kylacamelon@gmail.com>
2025-04-03 23:12:10 +0800

    second commit

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); //Allows parsing of JSON request
+
+const items = [
+    {id: 1, name: "Item 1"},
+    {id: 2, name: "Item 2"},
+    {id: 3, name: "Item 3"},
+];
+
+// GET
+app.get('/api/items', (req, res) =>{
+    res.json(items);
+});
+
+//POST
+app.post('/api/items', (req, res) =>{
+    const newItem = {id: items.length +1, name: req.body.name};
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+// PUT
+app.put('/api/items', (req, res) =>{
+    const item = items.find(i => i.id === parseInt(req.params.id));
+    if (!item) return res.status(404).json({message: "Item not found"});
+    item.name = res.body.name;
+    res.json(item);
+});
+
+// DELETE
+app.delete('/api/items', (req, res) =>{
+    const item = items.findIndex(i => i.id === parseInt(req.params.id));
+    if (idex === -1) return res.status(404).json({message: "Item not found"});
+
+    items.splice(index, 1);
+    res.json({message: "Item deleted"});
+
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+
